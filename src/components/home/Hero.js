2503ac3bcb614024f835f57f0d18110f3b8e7d37
sanskar-775 <sharmasanskar840@gmail.com>
2025-03-21 02:06:39 +0530

    Added Some UI ,AboutUS page, Changed theme

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -4,10 +4,14 @@ import Link from "next/link";
 export default function Hero() {
   return (
     <div className="hero-container flex flex-col md:flex-row items-center justify-between p-8 bg-black text-white">
-      <div className="hero-text flex flex-col items-center">
-        <h1 className="text-4xl font-bold">HackSphere</h1>
-        <p className="text-lg mt-2">Bringing Global Hackathons to One Place.</p>
-        {/*  <button className="mt-4 px-6 py-2 bg-blue-600 rounded"></button> */}
+      <div className="hero-text flex flex-col items-center relative">
+        <h1 className="text-5xl font-bold relative z-10 glow-gradient-text">HackSphere</h1>
+        <h1 
+          className="text-5xl font-bold absolute top-[20%] left-[15.87%] w-full opacity-40 transform scale-y-[-1] mask-gradient"
+        >
+          HackSphere
+        </h1>
+        <p className="text-lg mt-2 p-8">Bringing Global Hackathons to One Place.</p>
         <Link href="/events" className="btn-outline mt-4 rounded">Explore Events</Link>
       </div>
       
